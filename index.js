43386a8c7a295646178a7ab26f17e50179d7f9e0
Feyi <anjuwontobi99@gamil.com>
2022-09-03 18:10:01 +0100

    Using the calculate function

--- index.js
+++ index.js
@@ -5,13 +5,35 @@ const allSymbols = ["+", "-", "X", "+", "%", "C", "="];
 let firstValue = "";
 let secondValue = "";
 let symbol = "";
+let result = "";
+
+const calculate = () => {
+  firstValue = parseFloat(firstValue);
+  secondValue = parseFloat(secondValue);
+  if (symbol === "+") result = firstValue + secondValue;
+  if (symbol === "-") result = firstValue - secondValue;
+  if (symbol === "÷") result = firstValue / secondValue;
+  if (symbol === "X") result = firstValue * secondValue;
+  if (symbol === "%") result = firstValue % secondValue;
+
+  display.innerText = result;
+  firstValue = result;
+  secondValue = "";
+};
 
 for (let button of controlButtons) {
   button.addEventListener("click", () => {
     const { innerText: btnValue } = button;
     const btnValueIsSymbol = allSymbols.includes(btnValue);
 
+    if (!secondValue && btnValue === "=") return null;
+    if (btnValue === "C") {
+      firstValue = secondValue = symbol = "";
+      return (display.innerText = "");
+    }
+
     if (firstValue && btnValueIsSymbol) {
+      secondValue && calculate();
       symbol = btnValue;
     } else if (!symbol) firstValue += btnValue;
     else if (symbol) secondValue += btnValue;
