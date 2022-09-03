dd04e378f8c5a4ad227fe80890d855a82f8ff641
Feyi <anjuwontobi99@gamil.com>
2022-09-03 17:14:30 +0100

    Displaying value on my calculator with Js

---
+++ index.js
@@ -0,0 +1,21 @@
+const display = document.querySelector(".display");
+const controlButtons = document.querySelector(".controls").children;
+const allSymbols = ["+", "-", "X", "+", "%", "C", "="];
+
+let firstValue = "";
+let secondValue = "";
+let symbol = "";
+
+for (let button of controlButtons) {
+  button.addEventListener("click", () => {
+    const { innerText: btnValue } = button;
+    const btnValueIsSymbol = allSymbols.includes(btnValue);
+
+    if (firstValue && btnValueIsSymbol) {
+      symbol = btnValue;
+    } else if (!symbol) firstValue += btnValue;
+    else if (symbol) secondValue += btnValue;
+
+    if (btnValue !== "=") display.innerText += btnValue;
+  });
+}
